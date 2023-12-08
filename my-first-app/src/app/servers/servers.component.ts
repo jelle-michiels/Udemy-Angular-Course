9101a1bb487db09d29e4b994ec988135f7508a8f
jelle-michiels <jelle.michiels1@student.ucll.be>
2023-12-08 20:19:00 +0100

    lecture 28 done

--- my-first-app/src/app/servers/servers.component.ts
+++ my-first-app/src/app/servers/servers.component.ts
@@ -3,11 +3,17 @@ import {Component} from '@angular/core';
 @Component({
   // selector: '[app-servers]',
   // selector: '.app-servers',
-  selector: 'app-servers',
-  template: `
-    <app-server></app-server>
-    <app-server></app-server>`,
-  styleUrls: ['./servers.component.css']
+  selector: 'app-servers', // template: `
+  //   <app-server></app-server>
+  //   <app-server></app-server>`,
+  templateUrl: 'servers.component.html', styleUrls: ['./servers.component.css']
 })
 export class ServersComponent {
+  allowNewServer = false;
+
+  constructor() {
+    setTimeout(() => {
+      this.allowNewServer = true
+    }, 2000);
+  }
 }
