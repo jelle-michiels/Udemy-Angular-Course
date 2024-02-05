161ed5efb194ad3a71be9f1c99f1b9e2f72f40dc
jelle-michiels <jelle.michiels1@student.ucll.be>
2024-02-05 13:17:27 +0100

    Assignment 3 done

---
+++ basics-assignment-3-start/src/app/app.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent {
+  displayDetails = false;
+  numbers = [];
+
+  changeDisplay() {
+    this.displayDetails = !this.displayDetails;
+    this.numbers.push(this.numbers.length + 1);
+  }
+}
+
+
