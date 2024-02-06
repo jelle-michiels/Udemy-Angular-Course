edde9ba68e23071eb6aa54a85204ca37ae8a5746
jelle-michiels <jelle.michiels1@student.ucll.be>
2024-02-06 15:41:38 +0100

    Section 6 - 88 - Adding Navigation with Event Binding and ngIf

--- course-project/src/app/app.component.ts
+++ course-project/src/app/app.component.ts
@@ -7,4 +7,9 @@ import {Component} from '@angular/core';
 })
 
 export class AppComponent {
+  selected: string = 'recipes';
+
+  onNavigate(selected: string) {
+    this.selected = selected;
+  }
 }
