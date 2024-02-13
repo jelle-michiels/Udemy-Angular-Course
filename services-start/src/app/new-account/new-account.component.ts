d080990948083cf9758686ba13d9aecd1b20c36e
jelle-michiels <jelle.michiels1@student.ucll.be>
2024-02-13 10:06:24 +0100

    Section 9 - 116 - Using Services for Cross-Component Communication

--- services-start/src/app/new-account/new-account.component.ts
+++ services-start/src/app/new-account/new-account.component.ts
@@ -10,10 +10,11 @@ import {AccountsService} from "../accounts.service";
 })
 export class NewAccountComponent {
   constructor(private loggingService: LoggingService, private accountService: AccountsService) {
+    this.accountService.statusUpdated.subscribe((status: string) => alert('New status: ' + status));
   }
 
   onCreateAccount(accountName: string, accountStatus: string) {
     this.accountService.addAccount(accountName, accountStatus)
-    this.loggingService.logStatusChange(accountStatus)
+    // this.loggingService.logStatusChange(accountStatus)
   }
 }
