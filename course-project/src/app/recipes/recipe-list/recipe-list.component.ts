860f5a2db00be6f7352aaf4c63785f0c871cec95
jelle-michiels <jelle.michiels1@student.ucll.be>
2024-02-14 09:38:39 +0100

    Section 10 - 122 - Using a Service for Cross-Component Communication

--- course-project/src/app/recipes/recipe-list/recipe-list.component.ts
+++ course-project/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,20 +1,19 @@
-import {Component, EventEmitter, Output} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {Recipe} from "../recipe.model";
+import {RecipeService} from "../recipe.service";
 
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrl: './recipe-list.component.css'
 })
-export class RecipeListComponent {
-  recipes: Recipe[] = [new Recipe('A Test Recipe', 'This is simply a test', 'https://i0.wp.com/picjumbo.com/wp-content/uploads/adding-apple-to-make-christmas-gingerbread-cookies-more-soft-free-photo.jpg?w=2210&quality=70'),
-    new Recipe('Another Test Recipe', 'This is simply a test', 'https://i0.wp.com/picjumbo.com/wp-content/uploads/adding-apple-to-make-christmas-gingerbread-cookies-more-soft-free-photo.jpg?w=2210&quality=70')];
-  @Output() recipeSelected = new EventEmitter<Recipe>()
+export class RecipeListComponent implements OnInit {
+  recipes: Recipe[];
 
-  constructor() {
+  constructor(private recipeService: RecipeService) {
   }
 
-  onRecipeSelected(recipe: Recipe) {
-    this.recipeSelected.emit(recipe)
+  ngOnInit() {
+    this.recipes = this.recipeService.getRecipes();
   }
 }
--- course-project/src/app/recipes/recipe.service.ts
+++ course-project/src/app/recipes/recipe.service.ts
@@ -1,9 +1,19 @@
-import { Injectable } from '@angular/core';
+import {EventEmitter, Injectable} from '@angular/core';
+import {Recipe} from "./recipe.model";
 
 @Injectable({
   providedIn: 'root'
 })
 export class RecipeService {
+  recipeSelected = new EventEmitter<Recipe>();
 
-  constructor() { }
+  private recipes: Recipe[] = [new Recipe('A Test Recipe', 'This is simply a test', 'https://i0.wp.com/picjumbo.com/wp-content/uploads/adding-apple-to-make-christmas-gingerbread-cookies-more-soft-free-photo.jpg?w=2210&quality=70'),
+    new Recipe('Another Test Recipe', 'This is simply a test', 'https://i0.wp.com/picjumbo.com/wp-content/uploads/adding-apple-to-make-christmas-gingerbread-cookies-more-soft-free-photo.jpg?w=2210&quality=70')];
+
+  constructor() {
+  }
+
+  getRecipes() {
+    return this.recipes.slice();
+  }
 }
