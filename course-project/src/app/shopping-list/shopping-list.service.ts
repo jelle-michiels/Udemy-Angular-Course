ddacc1533fb89d310109e4dca7182e6dd428a55e
jelle-michiels <jelle.michiels1@student.ucll.be>
2024-02-14 09:51:13 +0100

    Section 10 - 124 - Using Services for Pushing Data from A to B

--- course-project/src/app/shopping-list/shopping-list.service.ts
+++ course-project/src/app/shopping-list/shopping-list.service.ts
@@ -5,7 +5,7 @@ import {Ingredient} from "../shared/ingredient.model";
   providedIn: 'root'
 })
 export class ShoppingListService {
-  addedIngredient = new EventEmitter<Ingredient>();
+  ingredientsChanged = new EventEmitter<Ingredient[]>();
 
   private ingredients: Ingredient[] = [
     new Ingredient('Apples', 5),
@@ -21,5 +21,6 @@ export class ShoppingListService {
 
   addIngredient(ingredient: Ingredient) {
     this.ingredients.push(ingredient)
+    this.ingredientsChanged.emit(this.ingredients.slice())
   }
 }
