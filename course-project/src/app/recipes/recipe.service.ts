15a63c221a0795e6bc25b6b6f9f495883e1501a1
jelle-michiels <jelle.michiels1@student.ucll.be>
2024-02-14 10:03:39 +0100

    Section 10 - 125 - Adding Ingredients to Recipes

--- course-project/src/app/recipes/recipe.service.ts
+++ course-project/src/app/recipes/recipe.service.ts
@@ -1,5 +1,6 @@
 import {EventEmitter, Injectable} from '@angular/core';
 import {Recipe} from "./recipe.model";
+import {Ingredient} from "../shared/ingredient.model";
 
 @Injectable({
   providedIn: 'root'
@@ -7,8 +8,16 @@ import {Recipe} from "./recipe.model";
 export class RecipeService {
   recipeSelected = new EventEmitter<Recipe>();
 
-  private recipes: Recipe[] = [new Recipe('A Test Recipe', 'This is simply a test', 'https://i0.wp.com/picjumbo.com/wp-content/uploads/adding-apple-to-make-christmas-gingerbread-cookies-more-soft-free-photo.jpg?w=2210&quality=70'),
-    new Recipe('Another Test Recipe', 'This is simply a test', 'https://i0.wp.com/picjumbo.com/wp-content/uploads/adding-apple-to-make-christmas-gingerbread-cookies-more-soft-free-photo.jpg?w=2210&quality=70')];
+  private recipes: Recipe[] = [
+    new Recipe('Tasty Schnitzel',
+      'This is simply a test',
+      'https://www.fearlessdining.com/wp-content/uploads/2020/12/gluten-free-schnitzel-hero-1200.jpg',
+      [new Ingredient('Meat', 1), new Ingredient('French Fries', 20)]),
+    new Recipe('Big Fat Burger',
+      'What else do you need to say?',
+      'https://media.istockphoto.com/id/1498243668/fr/photo/cheeseburger-savoureux-avec-laitue-fromage-cheddar-tomate-et-cornichons-petit-pain-burger-aux.jpg?s=2048x2048&w=is&k=20&c=TFzcKdAbYk2mpIFIkybE-WhMuyXRHMonIbDaiQhY6Cc=',
+      [new Ingredient('Buns', 2), new Ingredient('Meat', 1), new Ingredient('Tomato', 1), new Ingredient('Lettuce', 1), new Ingredient('Cheese', 1)])
+  ];
 
   constructor() {
   }
