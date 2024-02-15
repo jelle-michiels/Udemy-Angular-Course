764d42f8ca0379de5ac6e8b390a12fd76716e285
jelle-michiels <jelle.michiels1@student.ucll.be>
2024-02-15 10:07:41 +0100

    Section 11 - 137 - Fetching Route Parameters

--- routing-start/src/app/app.module.ts
+++ routing-start/src/app/app.module.ts
@@ -15,7 +15,7 @@ import {RouterModule, Routes} from "@angular/router";
 
 const appRoutes: Routes = [
   {path: 'users', component: UsersComponent},
-  {path: 'users/:id', component: UserComponent},
+  {path: 'users/:id/:name', component: UserComponent},
   {path: 'servers', component: ServersComponent},
   {path: '', component: HomeComponent}
 ];
