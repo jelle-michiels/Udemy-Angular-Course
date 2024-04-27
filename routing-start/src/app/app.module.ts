278dbd7b17898c4f4b1facea2c73315c917f72d2
jelle-michiels <jelle.michiels1@student.ucll.be>
2024-04-27 20:17:54 +0200

    Section 11 - 146 - Redirecting and wildcard routes

--- routing-start/src/app/app.module.ts
+++ routing-start/src/app/app.module.ts
@@ -12,36 +12,22 @@ import {EditServerComponent} from './servers/edit-server/edit-server.component';
 import {ServerComponent} from './servers/server/server.component';
 import {ServersService} from './servers/servers.service';
 import {RouterModule, Routes} from "@angular/router";
+import {PageNotFoundComponent} from './page-not-found/page-not-found.component';
 
-const appRoutes: Routes = [
-  {
-    path: 'users', component: UsersComponent, children: [
-      {path: ':id/:name', component: UserComponent},
-    ]
-  },
-  {
-    path: 'servers', component: ServersComponent, children: [
-      {path: ':id', component: ServerComponent},
-      {path: ':id/edit', component: EditServerComponent},]
-  },
-  {path: '', component: HomeComponent}
-];
+const appRoutes: Routes = [{path: '**', redirectTo: '/not-found'}, {
+  path: 'users', component: UsersComponent, children: [{path: ':id/:name', component: UserComponent},]
+}, {
+  path: 'servers',
+  component: ServersComponent,
+  children: [{path: ':id', component: ServerComponent}, {path: ':id/edit', component: EditServerComponent},]
+}, {path: 'not-found', component: PageNotFoundComponent}, {path: '', component: HomeComponent}, {
+  path: '**',
+  redirectTo: '/not-found'
+},];
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HomeComponent,
-    UsersComponent,
-    ServersComponent,
-    UserComponent,
-    EditServerComponent,
-    ServerComponent
-  ],
-  imports: [
-    BrowserModule,
-    FormsModule,
-    RouterModule.forRoot(appRoutes)
-  ],
+  declarations: [AppComponent, HomeComponent, UsersComponent, ServersComponent, UserComponent, EditServerComponent, ServerComponent, PageNotFoundComponent],
+  imports: [BrowserModule, FormsModule, RouterModule.forRoot(appRoutes)],
   providers: [ServersService],
   bootstrap: [AppComponent]
 })
