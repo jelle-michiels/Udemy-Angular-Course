dbfb575ade28739b2ec2c69158b27269759a2d12
jelle-michiels <jelle.michiels1@student.ucll.be>
2024-05-04 21:20:15 +0200

    Section 12 - 160 - Adding navigation to the app

--- course-project/src/app/header/header.component.ts
+++ course-project/src/app/header/header.component.ts
@@ -1,15 +1,9 @@
-import {Component, EventEmitter, Output} from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
 })
-
 export class HeaderComponent {
   collapsed = true;
-  @Output() selected = new EventEmitter<string>();
-
-  changeSelected(selected: string) {
-    this.selected.emit(selected);
-  }
 }
